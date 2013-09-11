ce0f4cfcf905c0f0fdac82881e143d31fb7a7f5f
Stijn van Schooten <s.vanschooten@student.tudelft.nl>
2013-09-11 03:26:20 +0200

    improved functionality

--- js/fade.js
+++ js/fade.js
@@ -3,23 +3,22 @@
 */
 (function ( $ ) {
 	var F = $.fade = {};
+	F.target = null;
     F.defaults = {
 		"nav-color": '#a6a6a6',
+		"nav-color-orig": $(".fade-nav  li").css('background-color'),
 		"nav-color-text": '#ffffff',
+		"nav-color-text-orig": $(".fade-nav  li").css('color'),
 		"title-color": '#313131',
 		"title-color-text": '#ffffff',
 		"content-color": '#ffffff',
-		"fade-time": 650,
+		"fade-time": 350,
 		ease: "linear",
 		develop: true,
 		header: ""
 	}
 	F.navigators = {};
 	F.home = null;
-    F.fade = function(el, target) {
-        this.css( "color", shade );
-        return this;
-    };
 	$(function(){
 		F.defaults.header = $("div.fade-header").text();
 		$("div.fade-content div.fade-page").each(function(){
@@ -33,35 +32,54 @@
 			}
 			F.navigators[elem.attr('data-link')] = elem;
 		});
-		$("div.fade-nav ul li").each(function(){
+		dev('Built up navigation array');
+		$("div.fade-nav li").each(function(){
 			var elem = $(this);
-			elem.hover(function(){
-				elem.animate({
-					color: F.defaults['nav-color-text'],
-					backgroundColor: F.defaults['nav-color']
-				}, 
-				F.defaults['fade-time'], 
-				F.defaults.ease)
-				console.log(elem[0]);
-			});
+			var target = elem.attr('data-target');
+			elem.hover(
+				function(){
+					dev('animating on hover: ' + target);
+					elem.stop().animate({
+						color: F.defaults['nav-color-text'],
+						backgroundColor: F.defaults['nav-color'],
+						queue: false
+					}, 
+					F.defaults['fade-time'], 
+					F.defaults.ease)
+					elem.attr('anim', true);
+				},
+				function(){
+					dev('animating on hover stop: ' + target);
+					elem.stop().animate({
+						color: F.defaults['nav-color-text-orig'],
+						backgroundColor: F.defaults['nav-color-orig'],
+						queue: false
+					}, 
+					F.defaults['fade-time'], 
+					F.defaults.ease)
+				}
+			);
 				
 			elem.click(function(){
+				dev('fading to ' + target);
 				$("div.fade-page").fadeOut({
 					duration: F.defaults['fade-time'],
 					easing: F.defaults.ease,
-					complete: function(){
-						F.navigators[elem.attr('data-target')].fadeIn();
-					}
 				});
+				F.target = target;
+				setTimeout(function(){
+					F.navigators[F.target].fadeIn(F.defaults['fade-time'],
+						function(){
+							F.target = null;
+					});
+				}, F.defaults['fade-time'] + 10);
 			});
 		});
 	});
-}( jQuery ));
-
-
-
-function dev(msg){
-	if($.fade.defaults.develop){
-		console.log(msg);
+	
+	function dev(msg){
+		if($.fade.defaults.develop){
+			console.log(msg);
+		}
 	}
-}
+}( jQuery ));
